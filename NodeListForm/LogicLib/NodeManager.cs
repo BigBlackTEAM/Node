58119013062ae08790c1839e0028f5be60386f18
Crustbrns <student@25-09>
2021-07-02 11:08:35 +0300

    Add Node class

---
+++ NodeListForm/LogicLib/NodeManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLib
+{
+    public class NodeManager
+    {
+        public List<Node> Nodes { get; set; } = new List<Node>();
+
+
+    }
+}
