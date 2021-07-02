05c63806aab7236691e7b5fda0d1b2893c159e8d
crustbrns <student@25-11>
2021-07-02 11:08:12 +0300

    Created upper panel

--- NodeListForm/NodeListForm/Form1.Designer.cs
+++ NodeListForm/NodeListForm/Form1.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace NodeListForm
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NodeListForm
 {
     partial class Form1
     {
@@ -28,31 +32,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(462, 159);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(47, 53);
-            this.panel1.TabIndex = 0;
-            // 
-            // Form1
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
+            this.Name = "BigBlackNotes";
+            this.Text = "BigBlackNotes";
+            this.ForeColor = Color.FromArgb(255, 240, 240, 240);
+            this.ShowIcon = false;
+            this.BackColor = Color.FromArgb(255, 26, 26, 26);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.CenterScreen;
 
+            UpperPanel = new Panel()
+            {
+                Size = new Size(this.Width, 30),
+                Location = new Point(0, 0),
+                BackColor = Color.FromArgb(255, 26, 26, 26)
+            };
+
+            Border = new PictureBox()
+            {
+                Size = new Size(this.Width, 2),
+                Location = new Point(0, 30),
+                BackColor = Color.FromArgb(255, 0, 0, 0)
+            };
+
+            Buttons = new List<Button>();
+            Buttons.AddRange(new List<Button>()
+            {
+                new Button()
+                {
+                    Name = "Quit",
+                    Font = new Font("Consolas", 13),
+                    Text = "x",
+                    Size = new Size(30, 30),
+                    ForeColor = Color.FromArgb(255, 130, 130, 130),
+                    Location = new Point(this.Width-30,0),
+                    FlatStyle = FlatStyle.Flat,
+                    Anchor = AnchorStyles.Right,
+                    TextAlign = ContentAlignment.MiddleCenter
+                },
+                new Button()
+                {
+                    Name = "Maximize",
+                    Font = new Font("Consolas", 13),
+                    Text = "◻",
+                    Size = new Size(30, 30),
+                    ForeColor = Color.FromArgb(255, 130, 130, 130),
+                    Location = new Point(this.Width-60, 0),
+                    FlatStyle = FlatStyle.Flat,
+                    Anchor = AnchorStyles.Right,
+                    TextAlign = ContentAlignment.MiddleCenter
+                },
+                new Button()
+                {
+                    Name = "Minimize",
+                    Font = new Font("Consolas", 13),
+                    Text = "-",
+                    Size = new Size(30, 30),
+                    ForeColor = Color.FromArgb(255, 130, 130, 130),
+                    Location = new Point(this.Width-90, 0),
+                    FlatStyle = FlatStyle.Flat,
+                    Anchor = AnchorStyles.Right,
+                    TextAlign = ContentAlignment.MiddleCenter
+                }
+            });
+
+            Buttons.ForEach(x => UpperPanel.Controls.Add(x));
+            Buttons.ForEach(x => x.FlatAppearance.BorderSize = 0);
+            //Buttons.ForEach(x => x.MouseEnter += X_MouseEnter);
+            //Buttons.ForEach(x => x.MouseLeave += X_MouseLeave);
+            Buttons.ForEach(x => x.MouseClick += X_MouseClick);
+
+            Controls.Add(UpperPanel);
+            Controls.Add(Border);
         }
 
-        #endregion
+        private void X_MouseClick(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
 
-        private System.Windows.Forms.Panel panel1;
+            switch (button.Name)
+            {
+                case "Quit":
+                    this.Close();
+                    break;
+                case "Minimize":
+                    this.WindowState = FormWindowState.Minimized;
+                    break;
+                case "Maximize":
+                    if (this.WindowState != FormWindowState.Maximized)
+                        this.WindowState = FormWindowState.Maximized;
+                    else this.WindowState = FormWindowState.Normal;
+                    break;
+            }
+        }
+
+        private Panel UpperPanel;
+        private List<Button> Buttons;
+        private PictureBox Border;
+        #endregion
     }
 }
 
