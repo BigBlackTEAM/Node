b2c7441482e59365593c8f278b0916ddf525acb8
Crustbrns <student@25-09>
2021-07-02 11:36:17 +0300

    Added Node Manager

--- NodeListForm/LogicLib/NodeManager.cs
+++ NodeListForm/LogicLib/NodeManager.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LogicLib
-{
-    public class NodeManager
+
+    namespace LogicLib
     {
-        public List<Node> Nodes { get; set; } = new List<Node>();
+        [Serializable]
+        public class NodeManager
+        {
+            public List<Node> Nodes { get; set; } = new List<Node>();
+            public string Path { get; set; }
 
+            public NodeManager()
+            {
 
+            }
+
+            public NodeManager(List<Node> nodes, string path)
+            {
+                Nodes = nodes;
+                Path = path;
+            }
+
+            public void OrderByNameDecreasing()
+            {
+                Nodes = Nodes.OrderBy(x => x.Name).ToList();
+            }
+
+            public void OrderByNameIncreasing()
+            {
+                OrderByNameDecreasing();
+
+                Nodes.Reverse();
+            }
+
+        }
     }
-}
