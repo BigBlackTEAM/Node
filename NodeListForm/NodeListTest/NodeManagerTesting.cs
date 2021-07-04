31de00c88c57e42692af63995821816558231162
k3sha-MTM <den123456721@ua.fm>
2021-07-04 14:54:12 +0300

    add Test In logic

---
+++ NodeListForm/NodeListTest/NodeManagerTesting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+namespace NodeListTest
+{
+    public class NodeManagerTesting
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void NodeManagerTest()
+        {
+            Assert.IsNotNull(true);
+            Assert.IsNull(false);
+        }
+        [Test]
+        public void OrderByNameDecreasingTest()
+        {
+            Assert.IsTrue(false);
+            Assert.IsFalse(true);
+            Assert.IsNotNull(false);
+        }
+        [Test]
+        public void OrderByCreationTimeDecreasingTest()
+        {
+            Assert.IsTrue(false);
+            Assert.IsNotNull(false);
+            Assert.IsFalse(true);
+        }
+        [Test]
+        public void OrderByNameIncreasingTest()
+        {
+            Assert.IsTrue(false);
+            Assert.IsNotNull(false);
+            Assert.IsFalse(true);
+        }
+        [Test]
+        public void OrderByChangeTimeDecreasingTest()
+        {
+            Assert.IsTrue(false);
+            Assert.IsNotNull(false);
+            Assert.IsFalse(true);
+        }
+        [Test]
+        public void OrderByChangeTimeIncreasingTest()
+        {
+            Assert.IsTrue(false);
+            Assert.IsNotNull(false);
+            Assert.IsFalse(true);
+        }
+       
+    }
+}
