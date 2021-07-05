1dc5a667ec127f2e4a8aad901137947a1bce959c
Tanto <zr380678644997@gmail.com>
2021-07-05 19:18:05 +0300

    Add Property(FileName),for Class Loging,And Func(CreateFile)

--- NodeListForm/LogicLib/Loging.cs
+++ NodeListForm/LogicLib/Loging.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,17 @@ namespace LogicLib
     static class Loging
     {
 
-        
+        public static string FileName { get; private set; }
+        public static void CreateFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName).Close();
+            }
+            FileName = fileName;
+        }
 
-        //YA Lox
+
 
 
     }
