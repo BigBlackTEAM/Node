6266d80398eb1dc8ba0be2c66a5229b5a0e57e9f
Tanto <zr380678644997@gmail.com>
2021-07-05 19:15:51 +0300

    Add Enum For Loging

---
+++ NodeListForm/LogicLib/EnumTypesOfLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLib
+{
+    public enum LogType
+    {
+        ERROR = 1,
+        WARNING = 2,
+        MESSAGE = 3
+    }
+}
